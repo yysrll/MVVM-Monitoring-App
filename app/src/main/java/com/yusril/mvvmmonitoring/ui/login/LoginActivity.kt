3dd1efa9433e4f96643c764345ecdde4f227a105
yysrll <yusril.uzumaki@gmail.com>
2022-07-09 14:04:09 +0800

    layout: create login page

---
+++ app/src/main/java/com/yusril/mvvmmonitoring/ui/login/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.yusril.mvvmmonitoring.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yusril.mvvmmonitoring.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+    }
+}
